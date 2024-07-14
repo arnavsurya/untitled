ab0e900c36f6b10fbd9ebc7bf0abde6e00e45712
Arnav Suryavanshi <106728253+arnavsurya@users.noreply.github.com>
2024-07-14 15:58:46 +0530

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 c 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 c 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 c 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 c 
+7 7 7 7 7 7 7 7 7 7 7 7 7 6 6 c 
+7 7 7 6 7 7 7 7 7 7 7 6 6 6 6 c 
+7 6 7 6 6 7 7 6 7 7 6 d 6 6 6 c 
+6 6 7 6 6 6 6 d 6 6 6 d 6 6 b b 
+6 6 6 6 6 6 d d 6 6 d d d d b b 
+d d d d d d d d d d d d d b b b 
+d d d d d d d d d d d d d b b . 
+1 1 1 1 d d d d d d 1 1 b b . . 
+. . 1 1 1 d d d d 1 1 . . . . . 
+. . . . 1 1 1 1 1 . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
